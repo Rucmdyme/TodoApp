ebf1a6f31104d4e05d915be77bbe8d1dc6ce3b1f
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Air.local>
2023-06-19 15:24:18 +0530

    Commit Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const clickHandler = () => {
+    alert('Hii..')
+  };
+  const changeState = () => {
+    setText({ ...text, profile: 'FrontEnd' })
+  };
+  const [text, setText] = useState({
+    name: 'Abhishek',
+    age: 22,
+    profile: 'SD1'
+  });
+  const changeCount = () => {
+    setCount(count + 1)
+  };
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Image />
+      <Person />
+      <Greeting />
+      <button type='button' onClick={clickHandler}>
+        Click Here
+      </button>
+      <h2>{text.name}</h2>
+      <p>{text.age}</p>
+      <p>{text.profile}</p>
+      <button type='button' onClick={changeState}>
+        Change
+      </button>
+      <p>{count}</p>
+      <button type='button' onClick={changeCount}>
+        Counter
+      </button>
+    </>
   );
 }
 
+const Person = () => <h2>Abhishek</h2>
+const Greeting = () => (
+  <p>Hello There</p>
+);
+const Image = () => (
+  <img src='https://images.pexels.com/photos/17202898/pexels-photo-17202898/free-photo-of-city-street-building-house.jpeg' width={'200px'} alt=' ̰' />
+);
 export default App;
